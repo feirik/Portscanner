cc37f33f2df31afdec33f628018fe0417d8f589b
feirik <feirik@gmail.com>
2021-07-05 09:33:51 -0400

    Minor description fix

--- portscanner.py
+++ portscanner.py
@@ -3,7 +3,7 @@ import portscannerLib
 
 parser = argparse.ArgumentParser(description='Simple portscanner.')
 parser.add_argument("-p", "--port", type=str, help="Ports to scan")
-parser.add_argument("-T", "--timeout", type=float, help="Timeout when attempting to connect to a port")
+parser.add_argument("-T", "--timeout", type=float, help="Timeout in seconds when attempting to connect to a port")
 args, target = parser.parse_known_args()
 
 portscannerLib.scan(target[0], args.port, args.timeout)
